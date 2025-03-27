4c26cb0ce9e6fe072ce99e4fa1b89cb87ae83750
VladaParkhomenko <252503@edu.p.lodz.pl>
2025-03-27 14:46:44 +0100

    4.2 Poprawa nazw metod.

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,33 +2,33 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] number = new int[12];
+	private int[] number = new int[12];
 
-    public int total = -1;
+	public int total = -1;
 
-    public void countIn(int in) {
-        if (!isFull())
-            number[++total] = in;
-    }
+	public void countIn(int in) {
+		if (!isStackFull())
+			number[++total] = in;
+	}
 
-    public boolean callCheck() {
-        return total == -1;
-    }
+	public boolean callCheck() {
+		return total == -1;
+	}
 
-    public boolean isFull() {
-        return total == 11;
-    }
+	public boolean isStackFull() {
+		return total == 11;
+	}
 
-    protected int peekaboo() {
-        if (callCheck())
-            return -1;
-        return number[total];
-    }
+	protected int peekaboo() {
+		if (callCheck())
+			return -1;
+		return number[total];
+	}
 
-    public int countOut() {
-        if (callCheck())
-            return -1;
-        return number[total--];
-    }
+	public int countOut() {
+		if (callCheck())
+			return -1;
+		return number[total--];
+	}
 
 }
--- src/main/java/edu/kis/vh/nursery/FifoRhymer.java
+++ src/main/java/edu/kis/vh/nursery/FifoRhymer.java
@@ -2,18 +2,18 @@ package edu.kis.vh.nursery;
 
 public class FifoRhymer extends DefaultCountingOutRhymer {
 
-    public DefaultCountingOutRhymer temporaryRhymer = new DefaultCountingOutRhymer();
+	public DefaultCountingOutRhymer temporaryRhymer = new DefaultCountingOutRhymer();
 
-    @Override
-    public int countOut() {
-        while (!callCheck())
-            temporaryRhymer.countIn(super.countOut());
+	@Override
+	public int countOut() {
+		while (!callCheck())
+			temporaryRhymer.countIn(super.countOut());
 
-        int ret = temporaryRhymer.countOut();
+		int ret = temporaryRhymer.countOut();
 
-        while (!temporaryRhymer.callCheck())
-            countIn(temporaryRhymer.countOut());
+		while (!temporaryRhymer.callCheck())
+			countIn(temporaryRhymer.countOut());
 
-        return ret;
-    }
+		return ret;
+	}
 }
