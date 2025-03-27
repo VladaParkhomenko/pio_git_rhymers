4dedbd2358fa5c0ec24a61435627670b84ebd82b
VladaParkhomenko <252503@edu.p.lodz.pl>
2025-03-27 14:42:58 +0100

    4.1 Poprawa nazw klas i zmiennych.

---
+++ src/main/java/edu/kis/vh/nursery/FifoRhymer.java
@@ -0,0 +1,19 @@
+package edu.kis.vh.nursery;
+
+public class FifoRhymer extends DefaultCountingOutRhymer {
+
+    public DefaultCountingOutRhymer temporaryRhymer = new DefaultCountingOutRhymer();
+
+    @Override
+    public int countOut() {
+        while (!callCheck())
+            temporaryRhymer.countIn(super.countOut());
+
+        int ret = temporaryRhymer.countOut();
+
+        while (!temporaryRhymer.callCheck())
+            countIn(temporaryRhymer.countOut());
+
+        return ret;
+    }
+}
