780ff46e8cd65a4155f99244d2300e693113748c
VladaParkhomenko <252503@edu.p.lodz.pl>
2025-04-06 21:37:32 +0200

    14. Przeniesiono klasę Node jako klasę wewnętrzną IntLinkedList.

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,42 +1,67 @@
 package edu.kis.vh.nursery.list;
 
-public class IntLinkedList {
+class IntLinkedList {
 
 	private static final int ERROR = -1;
-	
-    Node last;
-    int i;
 
-    public void push(int i) {
-        if (last == null)
-            last = new Node(i);
-        else {
-            last.setNext(new Node(i));
-            last.getNext().setPrev(last);
-            last = last.getNext();
-        }
-    }
+	private Node last;
 
-    public boolean isEmpty() {
-        return last == null;
-    }
+	public void push(int i) {
+		if (last == null)
+			last = new Node(i);
+		else {
+			last.setNext(new Node(i));
+			last.getNext().setPrev(last);
+			last = last.getNext();
+		}
+	}
 
-    public boolean isFull() {
-        return false;
-    }
+	public boolean isEmpty() {
+		return last == null;
+	}
 
-    public int top() {
-        if (isEmpty())
-            return ERROR;
-        return last.value;
-    }
+	public boolean isFull() {
+		return false;
+	}
 
-    public int pop() {
-        if (isEmpty())
-            return ERROR;
-        int ret = last.value;
-        last = last.getPrev();
-        return ret;
-    }
+	public int top() {
+		if (isEmpty())
+			return ERROR;
+		return last.value;
+	}
 
+	public int pop() {
+		if (isEmpty())
+			return ERROR;
+		int ret = last.value;
+		last = last.getPrev();
+		return ret;
+	}
+
+	private static class Node {
+
+		private final int value;
+		private Node prev;
+		private Node next;
+
+		public Node(int i) {
+			value = i;
+		}
+
+		public Node getPrev() {
+			return prev;
+		}
+
+		public void setPrev(Node prev) {
+			this.prev = prev;
+		}
+
+		public Node getNext() {
+			return next;
+		}
+
+		public void setNext(Node next) {
+			this.next = next;
+		}
+	}
 }
