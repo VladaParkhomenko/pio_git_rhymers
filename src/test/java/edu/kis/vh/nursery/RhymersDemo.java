e8a498c15adad558ee1fb5a4baafb0ac85fa494b
VladaParkhomenko <252503@edu.p.lodz.pl>
2025-04-06 21:31:15 +0200

    13. Refaktoryzacja RhymersDemo: wydzielenie metody testRhymers.

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -10,7 +10,12 @@ class RhymersDemo {
     public static void main(String[] args) {
         RhymersFactory factory = new DefaultRhymersFactory();
         
-        DefaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
+        testRhymers(factory);
+        
+    }
+
+	static void testRhymers(RhymersFactory factory) {
+		DefaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
                 factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
         
         for (int i = 1; i < 15; i++)
@@ -29,7 +34,6 @@ class RhymersDemo {
         
         System.out.println("total rejected is "
                 + ((HanoiRhymer) rhymers[3]).getRejectedCount());
-        
-    }
+	}
     
 }
