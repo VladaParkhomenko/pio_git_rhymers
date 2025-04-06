753b7a9920a87a965873201573f29c28b06e77ef
VladaParkhomenko <252503@edu.p.lodz.pl>
2025-04-06 22:14:50 +0200

    18. Dodano komentarze TODO w celu poprawy kodu w FifoRhymer.

--- src/main/java/edu/kis/vh/nursery/FifoRhymer.java
+++ src/main/java/edu/kis/vh/nursery/FifoRhymer.java
@@ -2,23 +2,24 @@ package edu.kis.vh.nursery;
 
 public class FifoRhymer extends DefaultCountingOutRhymer {
 
-	private final DefaultCountingOutRhymer temporaryRhymer = new DefaultCountingOutRhymer();
+    private final DefaultCountingOutRhymer temporaryRhymer = new DefaultCountingOutRhymer();
 
-	@Override
-	public int countOut() {
-		while (!callCheck())
-			getTemporaryRhymer().countIn(super.countOut());
+    @Override
+    public int countOut() {
+        // TODO: Consider adding a limit to the number of iterations to avoid potential infinite loops in case of stack issues.
+        while (!callCheck())  
+            getTemporaryRhymer().countIn(super.countOut());
 
-		int ret = getTemporaryRhymer().countOut();
+        int ret = getTemporaryRhymer().countOut();  
 
-		while (!getTemporaryRhymer().callCheck())
-			countIn(getTemporaryRhymer().countOut());
+        // TODO: Ensure the temporary rhymer is emptied after operation to avoid memory issues.
+        while (!getTemporaryRhymer().callCheck()) 
+            countIn(getTemporaryRhymer().countOut());
 
-		return ret;
-	}
+        return ret;
+    }
 
-	private DefaultCountingOutRhymer getTemporaryRhymer() {
-		return temporaryRhymer;
-	}
+    private DefaultCountingOutRhymer getTemporaryRhymer() {
+        return temporaryRhymer;
+    }
 }
-
